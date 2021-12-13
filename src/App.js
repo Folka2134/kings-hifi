e127a845628a70353f10ab1fef1407de19e8f69c
folka2134 <folka2134@gmail.com>
2021-12-13 23:22:44 +0000

    Updated banner and about page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import CoverComponent from './components/cover.component';
 import AboutComponent from './components/about.component';
+import BannerComponent from './components/banner.component';
 
 const App = () => {
   return (
     <div className="bg-[url('/src/images/main-background.jpg')] bg-cover bg-fixed">
       <CoverComponent />
       <AboutComponent />
+      <BannerComponent />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}",  ],  
   theme: {    
-    extend: {},
+    extend: {
+      colors: {
+        'main-yellow': '#F3D534',
+      },
+    },
   },  
   plugins: [],
 }
