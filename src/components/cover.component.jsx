e77cf5e9e65ced49f29ede09712ab4999a1fb8c5
folka2134 <folka2134@gmail.com>
2021-12-14 00:55:49 +0000

    Updated cover logo

--- src/components/cover.component.jsx
+++ src/components/cover.component.jsx
@@ -6,8 +6,7 @@ const CoverComponent = () => {
     return (
         <div className="h-screen bg-[url('/src/images/kingshifi-soundsystem2.png')] bg-top">
             <div className="h-screen flex justify-center items-center bg-black bg-opacity-95">
-                {/* <img className="cursor-pointer h-52  hover:animate-ping absolute " src={logo} alt="logo" /> */}
-                <img className="h-96 hover:animate-pulse cursor-pointer" src={logo} alt="logo" />
+                <img className="h-1/3 hover:animate-pulse cursor-pointer" src={logo} alt="logo" />
             </div>
         </div>
     );
