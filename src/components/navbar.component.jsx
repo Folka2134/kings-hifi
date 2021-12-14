34fff0eb5c0a9b89dd138e59c2a699c744c6a6fe
folka2134 <folka2134@gmail.com>
2021-12-14 23:50:43 +0000

    Updated navbar shadow

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavbarComponent = () => {
     return (
-        <div className="h-16 flex justify-center sticky top-0 bg-main-black bg-opacity-95 shadow-2xl shadow-main-green">
+        <div className="h-16 flex justify-center sticky top-0 bg-main-black bg-opacity-95 shadow-md shadow-main-black rounded-b-xl">
         
         </div>
     );
