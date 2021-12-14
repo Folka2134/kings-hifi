da4ba000a3237978f2f5c2f7a15481e7ac8a3bda
folka2134 <folka2134@gmail.com>
2021-12-14 23:46:43 +0000

    Updated titles to center below sm size

--- src/components/about.component.jsx
+++ src/components/about.component.jsx
@@ -7,7 +7,7 @@ const AboutComponent = () => {
                 <div className="grid items-center">
                     <img className="w-96 lg:w-full" src="https://i.pinimg.com/736x/08/da/0d/08da0d0270c4194b2eb3083e04383f12.jpg" alt="" />
                 </div>
-                <div className="row-end-1 mt-24 lg:row-auto">
+                <div className="text-center sm:text-left row-end-1 mt-24 lg:row-auto">
                     <h2 className="text-5xl font-raleway font-bold text-font-title-color p-8"><span className="underline underline-offset-8 decoration-4">KING</span>S-HIFI</h2>
                     <p className="font-arimo text-xl 2xl:pr-56">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
