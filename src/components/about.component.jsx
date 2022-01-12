1cb12d56d3c0ad434f4b945fc6d9775292d7695e
folka2134 <folka2134@gmail.com>
2022-01-12 17:45:44 +0000

    Updated images to scale with container size

--- src/components/about.component.jsx
+++ src/components/about.component.jsx
@@ -8,13 +8,13 @@ const AboutComponent = () => {
         <div className="about-container h-screen bg-[url('/src/images/backgrounds/wave-haikei(5).svg')] bg-center bg-cover lg:bg-fixed">
             <div className="h-screen grid grid-cols-1 grid-rows-2 gap-20 row-1 justify-items-center content-center p-12 md:grid-rows-none lg:grid-cols-2 lg:gap-24 2xl:p-40">
                 <div className="grid md:flex justify-center items-center">
-                    <img className="h-96 lg:h-3/4 rounded-3xl border-4 border-main-black" src={aboutPic} alt="Crew pic" />
+                    <img className="rounded-3xl border-4 border-main-black" src={aboutPic} alt="Crew pic" />
                 </div>
                 <div className="text-center sm:text-left row-end-1 lg:row-auto self-center">
                     <h2 className="text-5xl font-raleway font-bold text-font-title-color p-8"><span className="underline underline-offset-8 decoration-4 decoration-main-red">KINGS</span>-HIFI</h2>
                     <p className="font-arimo text-xl 2xl:pr-56">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
-            </div> 
+            </div>
         </div>
     );
 }
--- src/components/soundsystem.component.jsx
+++ src/components/soundsystem.component.jsx
@@ -18,11 +18,11 @@ const SoundsystemComponent = () => {
         <div className="about-container h-screen bg-[url('/src/images/backgrounds/wave-haikei(1).svg')] bg-center bg-cover lg:bg-fixed">
             <div className="h-screen grid grid-cols-1 grid-rows-2 gap-20 row-1 justify-items-center content-center p-12 md:grid-rows-none lg:grid-cols-2 lg:gap-24 xl:p-40">
                 <div className="row-end-1 text-center sm:text-right lg:row-auto 2xl:ml-52 self-center">
-                    <h2 className="text-5xl font-raleway font-bold text-font-title-color p-8">SOUND SYS<span className="underline underline-offset-8 decoration-4 decoration-main-red">TEM</span></h2>
-                    <p className="font-arimo text-xl 2xl:pl-56">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <h2 className="text-5xl font-raleway font-bold text-font-title-color p-8">SOUND-<span className="underline underline-offset-8 decoration-4 decoration-main-red">SYSTEM</span></h2>
+                    <p className="font-arimo text-xl 1xl:pl-56">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
                 <div className="grid md:flex justify-center items-center">
-                    <img className="h-96 lg:h-3/4 rounded-3xl border-4 border-main-black" src={soundsystem} alt="sound system" />
+                    <img className=" rounded-3xl border-4 border-main-black" src={soundsystem} alt="sound system" />
                 </div>
             </div>
         </div>
