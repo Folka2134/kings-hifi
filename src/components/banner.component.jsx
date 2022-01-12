720146c9e833047956032e1df15074bad737d7c2
folka2134 <folka2134@gmail.com>
2022-01-12 23:20:34 +0000

    Updated banner, Updated nav z-index

--- src/components/banner.component.jsx
+++ src/components/banner.component.jsx
@@ -14,12 +14,12 @@ const BannerComponent = () => {
     return (
         // <div className="h-52 flex justify-center group bg-main-yellow bg-opacity-95">         
 
-            <div className="h-52 flex justify-center group bg-gradient-to-b from-main-yellow to-main-green bg-opacity-95 border-2 border-main-black overflow-hidden lg:mt-0 lg:rounded-3xl">                
-                <img className="hover:animate-spin-slow" src={record1} alt="record 1"/>
-                <img className="group-hover:animate-spin-slow" src={record2} alt="record 2"/>
-                <img className="hover:animate-spin-slow" src={record3} alt="record 3"/>
-            </div>
-      
+        <div className="h-40 w-full flex justify-center group bg-gradient-to-b from-main-yellow to-main-green bg-opacity-95 border-2 border-main-black overflow-hidden md:rounded-3xl z-0 lg:h-52 lg:w-[45rem] lg:absolute lg:left-[10%] lg:mt-0">
+            <img src={record1} alt="record 1" />
+            <img src={record2} alt="record 2" />
+            <img src={record3} alt="record 3" />
+        </div>
+
     );
 }
 
--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -6,7 +6,7 @@ import logo from "../images/kingshifilogo2.png"
 
 const NavbarComponent = () => {
     return (
-        <div className="md:h-16 w-80 md:w-full justify-between md:flex sticky top-0 bg-main-black bg-opacity-95 shadow-md shadow-main-black rounded-b-xl">
+        <div className="md:h-16 w-80 md:w-full justify-between md:flex sticky top-0 bg-main-black bg-opacity-95 shadow-md shadow-main-black rounded-b-xl z-10">
             <GiHamburgerMenu size={50} className='md:hidden absolute m-2 text-white ' />
             <ul className='md:flex grid justify-center text-center items-center align-middle text-white'>
                 <li className='md:p-3'><img src={logo} alt="logo" className='h-16' /> </li>
