26ddda16551a3e07abb6237b531ab8b528e627e1
folka2134 <folka2134@gmail.com>
2022-01-13 01:12:47 +0000

    Updated about background

--- src/components/about.component.jsx
+++ src/components/about.component.jsx
@@ -5,7 +5,7 @@ import aboutPic from "../images/kingshifi-crew.jpg"
 
 const AboutComponent = () => {
     return (
-        <div className="about-container h-screen bg-[url('/src/images/backgrounds/wave-haikei(6).svg')] bg-center bg-cover lg:bg-fixed border-b-8 border-solid">
+        <div className="about-container h-screen bg-[url('/src/images/backgrounds/aboutBackground1.svg')] bg-center bg-cover lg:bg-fixed">
             <div className="h-screen grid grid-cols-1 grid-rows-2 gap-20 row-1 justify-items-center content-center p-12 md:grid-rows-none lg:grid-cols-2 lg:gap-24 2xl:p-40">
                 {/* <div className="grid md:flex justify-center items-center"> */}
                 <img className="max-h-[34rem] rounded-3xl border-4 border-main-black" src={aboutPic} alt="Crew pic" />
