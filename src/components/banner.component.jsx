ef2e9d56033598d130984b24dcc3ff7ffdef3f96
Folka2134 <folka2134@gmail.com>
2022-01-19 16:34:02 +0000

    Updated banner background

--- src/components/banner.component.jsx
+++ src/components/banner.component.jsx
@@ -7,10 +7,12 @@ import record3 from "../images/banner/vixonsoundleftbehindwhite.png"
 const BannerComponent = () => {
     return (
         // <div className="h-40 w-full flex justify-center group bg-gradient-to-b from-main-yellow to-main-green bg-opacity-10 border-2 border-main-black overflow-hidden md:rounded-3xl lg:h-52 lg:w-[45rem] lg:mx-[30%] lg:mt-0">
-        <div className="h-40 w-full flex justify-center group bg-main-green bg-opacity-80 border-2 border-main-black overflow-hidden md:rounded-b-3xl md:rounded-t-md lg:h-44 lg:w-[45rem] lg:mx-[30%] lg:mt-0 4xl:h-56">
-            <img src={record1} alt="record 1" />
-            <img src={record2} alt="record 2" />
-            <img src={record3} alt="record 3" />
+        <div className="bg-center bg-cover bg-[url('/src/images/banner/bannerbackground2.jpg')] bg-opacity-95 h-40 w-full flex justify-center group border-2 border-main-black overflow-hidden md:rounded-b-3xl md:rounded-t-md lg:h-44 lg:w-[45rem] lg:mx-[30%] lg:mt-0 4xl:h-56">
+            <div className=" bg-black h-40 w-full flex justify-center group bg-opacity-80 border-2 border-main-black overflow-hidden md:rounded-b-3xl md:rounded-t-md lg:h-44 lg:w-[45rem] lg:mt-0 4xl:h-56">
+                <img src={record1} alt="record 1" />
+                <img src={record2} alt="record 2" />
+                <img src={record3} alt="record 3" />
+            </div>
         </div>
     );
 }
